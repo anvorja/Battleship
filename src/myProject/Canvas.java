231f6060787ab9f9c4190ced6518522e1946b9e9
BorjaM <andres.vorja.vorja@gmail.com>
2023-01-26 06:16:21 -0500

    Canvas class

--- src/myProject/Canvas.java
+++ src/myProject/Canvas.java
@@ -1,4 +1,36 @@
 package myProject;
 
-public class Canvas {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
+/**
+ * @author Deisy Catalina Melo - deisy.melo@correounivalle.edu.co
+ *         Carlos Andrés Borja - borja.carlos@correounivalle.edu.co
+ * @version v
+ */
+public class Canvas  extends JPanel
+{
+    private ImageIcon imagenFrame;
+
+    /**
+     * Constructor of class Canvas
+     * */
+
+    public Canvas()
+    {
+        imagenFrame =
+                new ImageIcon(Objects.requireNonNull(getClass().getResource("/myProject/resources/inicio.png")));
+    }
+
+    /**
+     * This method is responsible for painting the background of the JFrame and the panelGame
+     * */
+    public void paintComponent(Graphics g) {
+
+        imagenFrame = new ImageIcon(imagenFrame.getImage().getScaledInstance(960, 581, Image.SCALE_SMOOTH));
+        g.drawImage(imagenFrame.getImage(), 0, 0, getWidth(), getHeight(), null);
+        setOpaque(false);
+        super.paintComponent(g);
+    }
 }
